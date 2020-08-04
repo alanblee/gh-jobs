76caf669c5b051f5b67cb84c60b30a412182362b
alanblee <alanbenlee12@gmail.com>
2020-08-04 10:10:24 -0700

    adding toggle nav btn

--- src/components/dashboard/navList.jsx
+++ src/components/dashboard/navList.jsx
@@ -3,42 +3,45 @@ import { Link } from "react-router-dom";
 
 const NavList = () => {
   return (
-    <div className="navigation">
-      <ul>
-        <li>
-          <Link to="/">
-            <span className="icon">
-              <i className="fa fa-home" aria-hidden="true"></i>
-            </span>
-            <span className="title">Home</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <span className="icon">
-              <i className="fa fa-user" aria-hidden="true"></i>
-            </span>
-            <span className="title">Profile</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <span className="icon">
-              <i className="fa fa-search" aria-hidden="true"></i>
-            </span>
-            <span className="title">Job Search</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <span className="icon">
-              <i className="fa fa-sign-out" aria-hidden="true"></i>
-            </span>
-            <span className="title">Logout</span>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="navigation">
+        <ul>
+          <li>
+            <Link to="/">
+              <span className="icon">
+                <i className="fa fa-home" aria-hidden="true"></i>
+              </span>
+              <span className="title">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className="icon">
+                <i className="fa fa-user" aria-hidden="true"></i>
+              </span>
+              <span className="title">Profile</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className="icon">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </span>
+              <span className="title">Job Search</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className="icon">
+                <i className="fa fa-sign-out" aria-hidden="true"></i>
+              </span>
+              <span className="title">Logout</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="toggle"></div>
+    </>
   );
 };
 
